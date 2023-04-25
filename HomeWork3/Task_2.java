4f6f4c90911c7bd91b6f044027902a36e4e13ae2
Vartan Shirinyan <02vartan-s@mail.ru>
2023-04-25 18:23:47 +0300

    Готово

---
+++ HomeWork3/Task_2.java
@@ -0,0 +1,32 @@
+package HomeWork3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/*
+Задан целочисленный список ArrayList. 
+Найти минимальное, максимальное и среднее арифметическое из этого списка. Collections.max() 
+*/
+public class Task_2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arraylist = new ArrayList<Integer>();
+        arraylist.add(1);
+        arraylist.add(2);
+        arraylist.add(3);
+        arraylist.add(4);
+        arraylist.add(5);
+
+        int min = Collections.min(arraylist);
+        int max = Collections.max(arraylist);
+
+        double sum = 0;
+        for (int i = 0; i < arraylist.size(); i++) {
+            sum += arraylist.get(i);
+        }
+        double average = sum / arraylist.size();
+
+        System.out.println("Минимальное: " + min);
+        System.out.println("Максимальное: " + max);
+        System.out.println("Среднее арифметическое: " + average);
+    }
+}
---
+++ HomeWork3/Task_3.java
@@ -0,0 +1,41 @@
+package HomeWork3;
+
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/*
+ Заполнить список названиями планет Солнечной системы в произвольном порядке с повторениями. 
+ Вывести название каждой планеты и количество его повторений в списке.
+ */
+public class Task_3 {
+    public static void main(String[] args) {
+        List<String> planets = new ArrayList<>();
+        planets.add("Земля");
+        planets.add("Венера");
+        planets.add("Земля");
+        planets.add("Марс");
+        planets.add("Марс");
+        planets.add("Юпитер");
+        planets.add("Меркурий");
+        planets.add("Сатурн");
+        planets.add("Уран");
+        planets.add("Нептун");
+        planets.add("Плутон");
+        planets.add("Земля");
+
+        Map<String, Integer> planetCount = new HashMap<>();
+        for (String planet : planets) {
+            Integer count = planetCount.get(planet);
+            if (count == null) {
+                count = 0;
+            }
+            planetCount.put(planet, count + 1);
+        }
+
+        for (Map.Entry<String, Integer> entry : planetCount.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
