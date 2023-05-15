aad2ea1710a3fce506478f167bcc80fdb581e357
VartanShirinyan <02vartan-s@mail.ru>
2023-05-16 00:08:27 +0300

    Готово

--- HomeWork1/Task_1.java
+++ HomeWork1/Task_1.java
@@ -1,24 +1,9 @@
 package HomeWork1;
 /* Вывести все простые числа от 1 до 1000 */
 public class Task_1 {
-    public static void main(String[] args) {
-        for (int i = 2; i <= 1000; i++) {
-            boolean isPrime = true;
-            for (int j = 2; j <= Math.sqrt(i); j++) {
-                if (i % j == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-            if (isPrime) {
-                System.out.print(i + " ");
-            }
-        }
-    }
-}
-package HomeWork1;
-/*Вывести все простые числа от 1 до 1000 */
-public class Task_1 {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         for (int i = 2; i <= 1000; i++) {
             boolean isPrime = true;
